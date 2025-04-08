d85f5ea892f5a586679850ea5895174d6ee258f1
dxnisse07 <denisse.gonzalez25@compscihigh.org>
2025-04-08 14:01:28 +0000

    hi

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
+const base = "https://bug-free-couscous-wrgrp5p6g54v255rg-3000.app.github.dev"
+
 // Create a function that tests the post route handler [2 pts]
-
-
+async function random() {
+    try {
+    const response= await fetch(base + "/",{
+        method: "POST",
+        headers: {
+             "Content-Type": "application/json"
+        },
+        body: JSON.stringify()
+    })
+}
+}
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
